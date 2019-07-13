95b311d1c5887c1834fab39131c5810a5cbed279
RavenVol <r.volianiuk@gmail.com>
2019-07-13 22:52:34 +0300

    Updates

---
+++ precache-manifest.3d33f1c0f3c3a6397a2f8880dec3e36f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8fbbfba7b2098c6516f8d6d1d70e118",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "7debbdb44a4c5ae8f104",
+    "url": "/react_list-of-goods/static/js/2.10afca9b.chunk.js"
+  },
+  {
+    "revision": "4b260e73619b90994174",
+    "url": "/react_list-of-goods/static/js/main.71bbc786.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_list-of-goods/precache-manifest.d1a60a28eda3e5f773fa801cba116629.js"
+  "/react_list-of-goods/precache-manifest.3d33f1c0f3c3a6397a2f8880dec3e36f.js"
 );
 
 self.addEventListener('message', (event) => {
